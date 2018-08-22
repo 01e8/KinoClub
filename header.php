2f4b7f9ed7b4ab16fd10253954daa75c81a09909
golubev-og <34693908+golubev-og@users.noreply.github.com>
2018-08-22 22:55:23 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,49 +1,59 @@
 <!DOCTYPE html>
 <html lang="ru">
     
-    
-    
     <head>
-        <title><?php bloginfo('name')?></title>
-        <link rel="shortcut icon" href="icon/ico.ico">
         <meta charset="utf-8">
+        <title><?php bloginfo('name') ?></title>
+        <link rel="shortcut icon" href="<?php bloginfo('template_url');?>/icon/ico.ico">
+        <meta name=viewport content="width=device-width, initial-scale=1.0 ">
         <meta name="author" Content="Голубев">
         <meta name="copyright" Content="GOLUBEV"> 
         <meta name="keywords" content="КИНОКЛУБ,КИНО,КЛУБ">
-        <meta name=viewport content="width=device-width, initial-scale=1.0 ">
-        <link rel="stylesheet" href="style.css" />  <!-- http://cinemaclub.moscow/wp-content/themes/Daggers/       ?php bloginfo('template_url');?>/ -->
+        
+        <?php wp_enqueue_script('jquery'); ?>
+        
+        <script language="javascript" type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.masonry.min.js"></script>
+        
+        <?php wp_head(); ?>
+        
+        <script type="text/javascript">
+        jQuery(document).ready(function($){
+            $(window).load(function () {
+                $('#content_masonry').masonry({ singleMode: true });
+            });
+        });
+        </script>
+        
     </head>
     
-    
-    
     <body>
-        <script type="text/javascript" src="script.js"></script>
+        <script type="text/javascript" src="<?php bloginfo('template_url');?>/script.js"></script>
 
-        
-        <!--Шапка сайта-->
         <header id="hat">
             <div id="videologo">
                 <div id="video_contain">
                 <video id="logovideo" autoplay loop muted>
-                    <source src="video/long.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
-                    <source src="video/long.webm" type='video/webm; codecs="vp8, vorbis"'>
-                    <source src="video/long.ogv" type='video/ogg; codecs="theora, vorbis"'>
+                    <source src="<?php bloginfo('template_url');?>/video/<?php echo rand(1, 3); ?>.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+                    <source src="<?php bloginfo('template_url');?>/video/<?php echo rand(1, 3); ?>.webm" type='video/webm; codecs="vp8, vorbis"'>
+                    <source src="<?php bloginfo('template_url');?>/video/<?php echo rand(1, 3); ?>.ogv" type='video/ogg; codecs="theora, vorbis"'>
                 </video>
                 </div>
                 <div id="logotext">
                     <a href="<?php echo home_url();?>">
-                        <img id="logoimg" src="img/logo.png">
-                        <img id="logoimgmini" src="img/logomini.png">
+                        <img id="logoimg" src="<?php bloginfo('template_url');?>/img/logo.png">
+                        <img id="logoimgmini" src="<?php bloginfo('template_url');?>/img/logomini.png">
                     </a>
                 </div>
                 <nav class="navnav">
+                    <?php wp_nav_menu(array('theme_location'=>'menu','container'=>'false','link_before'=>'<div class="menubutton">','link_after'=>'</div>')); ?>
+                    <!--
                     <ul class="navul">
-                        <a href="<?php echo home_url();?>"><li class="buttomfirst">ГЛАВНАЯ</li></a>
+                        <a href="<?php echo home_url();?>"><li>ГЛАВНАЯ</li></a>
                         <a href="@"><li>ВСТРЕЧИ</li></a>
                         <a href="@"><li>ЖУРНАЛ</li></a>
                         <a href="@"><li>УЧАСТНИКИ</li></a>
-                        <a href="@"><li class="buttomlast">ВСТУПАЙ</li></a>
-                    </ul>
+                        <a href="@"><li>ВСТУПАЙ</li></a>
+                    </ul>-->
                 </nav>
             </div>
         </header>
