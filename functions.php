d00c6edc9e0d90cf91b08397b94b1fa5d1f3a754
golubev-og <34693908+golubev-og@users.noreply.github.com>
2018-08-22 22:53:37 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,20 @@
 <?php
 
+function theme_name_scripts() {
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'theme_name_scripts');
+
+//Миниатюра
+add_theme_support( 'post-thumbnails' );
+
+//Меню
+register_nav_menu('menu','Меню в шапке');
+
 function mason_script() {
     wp_enqueue_script( 'jquery-masonry' );
 }
 add_action( 'wp_enqueue_scripts', 'mason_script' );
+
+?>
