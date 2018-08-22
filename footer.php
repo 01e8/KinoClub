10ce71593d2441d8b9afd4b4a64b4eae43c41dd3
golubev-og <34693908+golubev-og@users.noreply.github.com>
2018-08-22 22:54:45 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,14 @@
 
+        <footer id="down">
+                <nav class="navnavfooter">
+                    <?php wp_nav_menu(array('theme_location'=>'menu','container'=>'false','link_before'=>'<div class="menubutton">','link_after'=>'</div>')); ?>
+                </nav>
+                <div id="footerr">
+                    <p>© 2016 КИНОКЛУБ</p>
+                </div>
+        </footer>
+        
+        
+    <?php wp_footer(); ?>   
+    </body>
+</html>
